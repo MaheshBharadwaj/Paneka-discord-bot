cd7ba7116c0768b7acc1032be0d2dcbd67ec4559
Mahesh Bharadwaj <maheshbharadwaj101@gmail.com>
2020-07-16 16:20:59 +0530

    added team fixtures & improved documentation

--- source/bot_commands.py
+++ source/bot_commands.py
@@ -2,11 +2,12 @@ import discord
 import requests
 import json
 import os
-import datetime as dt
 
 from dotenv import load_dotenv
-from source.utils import putTableAll, putTableLong
+from source.utils import putTableAll, putTableLong,putFixtures,fetchJSON
 from source.league_code import LEAGUE_CODE
+from source.team_id import TEAM_ID
+from source.exceptions import *
 
 
 def getStandings(code, mode='long'):
@@ -14,148 +15,170 @@ def getStandings(code, mode='long'):
     Function that delivers standings in text format.
     Queries the cache for the requested data, if not found,
     Loads the data from API and caches it
-    """
-    load_dotenv()
 
-    API_KEY = os.getenv('API_KEY')
-    headers = {'X-Auth-Token': str(API_KEY)}
+    Parameters:
+    -----------
+    code: str
+        The ID of the league for which standings are required
+    mode: 'long' or 'all', optional
+        * defaults to 'long'
+        * 'long' -> SNO, Team name, Matches Played, Points Obtained
+        * 'all'  -> SNO, Team Code, Matches Played, Won, Drawn, Lost, Pts, Goal Difference
+
+    Returns:
+    --------
+    str
+        standings if code is valid, or an error message
+
+    """
 
     try:
-        assert(mode in ['long', 'all'])
-        leagueName = code
+        if code not in LEAGUE_CODE:
+            raise InvalidLeagueCodeException
 
-        if leagueName not in LEAGUE_CODE:
-            return "```diff\n- Please Check the league code!\
-                    \n  PL  - Premier League\
-                    \n  SPA - La Liga\
-                    \n  SA  - Serie A\
-                    \n  BL  - Bundesliga\
-                    \n  FL1 - Ligue One```"
-        else:
-            id = LEAGUE_CODE.get(leagueName)
-
-            #Checking Cache
-            if os.path.exists(f"cache/{leagueName}.json"):
-                print('Reading from cache :D')
-                with open(f"cache/{leagueName}.json", 'r') as cacheData:
-                    obj = json.load(cacheData)
-                    if mode == 'all':
-                        return putTableAll(obj)
-                    return putTableLong(obj)
-            else:
-
-                #Fetching data from API
-                r = requests.get(f"https://api.football-data.org/v2/competitions/{id}/standings", headers=headers)
-                obj = r.json()
-
-                #Writing data to cache
-                with open(f"cache/{leagueName}.json", 'w') as cachedata:
-                    json.dump(obj, cachedata, indent=4)
-
-                if mode == 'all':
-                    return putTableAll(obj)
-                return putTableLong(obj)
+        obj = fetchJSON(code, 'standings')
+        if mode == 'all':
+            return putTableAll(obj)
+        return putTableLong(obj)
                 
-    except AssertionError:
-        return '```diff\n- Invalid Args passed!```'
+    except InvalidLeagueCodeException:
+        return None
 
+
+def getFixtures(code, limit: int, mode):
+    """
+    Displays the fixtures in the requested league / team as an embed
+    Fetches fixtures from JSON file and renders embed for it,
+    Displays 'limit' matches
+    
+    Parameters:
+    -----------
+    code: str
+        The ID of the league or team for which fixtures are required
+    limit: int, optional
+        Number of fixtures to display (default value of 5)
+    mode: str ['league' or 'team']
+        Whether the fixtures of a league or a team are to be rendered
+
+    Returns:
+    --------
+    discord.Embed 
+        Shows the fixtures as many as requested,
+        Incase of invalid code, relevant help embed is returned
+
+    """
+    try:
+        if limit < 0:
+            raise InvalidLimitException
+
+        if (mode == 'league' and code not in LEAGUE_CODE):
+            raise InvalidLeagueCodeException
+        
+        if (mode == 'team' and code not in TEAM_ID):
+            raise InvalidTeamCodeException
+
+        obj = fetchJSON(code, 'fixtures')
+        return putFixtures(obj, code, limit, mode)
+
+                
+    except InvalidLimitException:
+        return discord.Embed(title = 'Limit must be greater than :zero:',\
+                            description="Enter a valid limit :smile:",\
+                            color=0xf58300 )
+
+    except InvalidLeagueCodeException:
+        return getLeagueCodes("Invalid League Code!")
+
+    except InvalidTeamCodeException:
+        return getTeamCodes("Invalid Team Code!")
+
+def getLeagueCodes(title="League Codes"):
+    """
+    Returns Leagues and their codes as an Embed
+
+    Parameters:
+    -----------
+    title: str, optional
+        Title of the embed (by default: "League Codes")
+
+    Returns:
+    --------
+    discord.Embed 
+        Embed displaying league codes
+
+    """
+
+    embed = discord.Embed(\
+                title=title,\
+                description="Refer codes for Top :five: Leagues here:",\
+                color=0xf58300)
+    embed.add_field(name=':one: Premier League', value = 'PL' + "\n\u200b", inline=False)
+    embed.add_field(name=':two: La Liga', value = 'SPA' + "\n\u200b", inline=True)
+    embed.add_field(name=':three: Serie A', value = 'SA' + "\n\u200b", inline=False)
+    embed.add_field(name=':four: Bundesliga', value = 'BA' + "\n\u200b", inline=True)
+    embed.add_field(name=':five:Ligue 1', value = 'FL1', inline=False)
+    return embed
+
+def getTeamCodes(title="Team Codes"):
+    """
+    Returns Teams and their codes as an Embed
+
+    Parameters:
+    -----------
+    title: str, optional
+        Title of the embed (by default: "Team Codes")
+
+    Returns:
+    --------
+    discord.Embed 
+        Embed displaying team codes
+
+    """
+
+    embed = discord.Embed(\
+                title=title,\
+                description="Refer codes for Top :one: :zero: Teams here:",\
+                color=0xf58300)
+    embed.add_field(name='Real Madrid', value = 'MAD' + "\n\u200b", inline=True)
+    embed.add_field(name='FC Barcelona', value = 'FCB' + "\n\u200b", inline=True)
+    embed.add_field(name='Manchester United', value = 'MUFC' + "\n\u200b", inline=True)
+    embed.add_field(name='Arsenal', value = 'AFC' + "\n\u200b", inline=True)
+    embed.add_field(name='Bayern Munich', value = 'BAY' + "\n\u200b", inline=True)
+    embed.add_field(name='Chelsea', value = 'CFC' + "\n\u200b", inline=True)
+    embed.add_field(name='Juventus', value = 'JUVE' + "\n\u200b", inline=True)
+    embed.add_field(name='Atletico Madrid', value = 'ATM' + "\n\u200b", inline=True)
+    embed.add_field(name='Liverpool', value = 'LFC' + "\n\u200b", inline=True)
+    embed.add_field(name='Manche$ter City', value = 'MCFC', inline=True)
+    return embed
 
 def getHelpEmbed(ctx):
     """
-    Generates the Help embed when
+    Generates the 'Help' embed when requested
+
+    Parameters:
+    -----------
+    ctx: discord.Context
+        Context data passed by discord when a command is invoked
+
+    Returns:
+    --------
+    discord.Embed 
+        Showing help data for the commands available
+
     """
     embed = discord.Embed(
                 title="Paneka-Help!",
                 description="Shows available commands and their functions",
                 color=0xf58300 )
     embed.set_thumbnail(url = "https://img.icons8.com/fluent/144/000000/get-help.png")
-    embed.add_field(name = ":exclamation: standings-all [league]", value = "Detailed Standings, with team codes", inline=False)
-    embed.add_field(name = ":exclamation: standings [league]", value = "Display Standings", inline=False)
-    embed.add_field(name = ":exclamation: fixtures [league] [limit (default: 5)]", value = "Displays Fixtures in League", inline=False)
+    embed.add_field(name = ":one: standings-all [league code]", value = "Detailed Standings, with team codes", inline=False)
+    embed.add_field(name = ":two: standings [league code]", value = "Display Standings", inline=False)
+    embed.add_field(name = ":three: fixtures ['league' or 'team'] [code] [limit (default: :five: )]", value = "Displays Fixtures", inline=False)
+    embed.add_field(name = ":four: league-codes", value = "Displays Leagues and their Respective Codes", inline=False)
+    embed.add_field(name = ":five: team-codes", value = "Displayes Teams and their Respectice Codes", inline=False)
     embed.set_footer(text='Requested By: ' + str(ctx.author))
 
     return embed
-
-
-
-def getFixtures(code, limit: int):
-    """
-    Displays the fixtures in the requested league as an embed
-    Fetches fixtures from JSON file and renders embed for it,
-    Displays 'limit' matches
-    """
-    load_dotenv()
-
-    API_KEY = os.getenv('API_KEY')
-    headers = {'X-Auth-Token': str(API_KEY)}
-
-    try:
-        assert(limit > 0)
-
-        leagueName = code
-
-        if leagueName not in LEAGUE_CODE:
-            embed = discord.Embed(\
-                title="Invalid League!",\
-                description="Refer codes for Top :five: Leagues here:",\
-                color=0xf58300)
-            embed.add_field(name='Premier League', value = 'PL' + "\n\u200b", inline=False)
-            embed.add_field(name='La Liga', value = 'SPA' + "\n\u200b", inline=True)
-            embed.add_field(name='Serie A', value = 'SA' + "\n\u200b", inline=False)
-            embed.add_field(name='Bundesliga', value = 'BA' + "\n\u200b", inline=True)
-            embed.add_field(name='Ligue 1', value = 'FL1', inline=False)
-            return embed
-
-        else:
-            id = LEAGUE_CODE.get(leagueName)
-
-            #Checking Cache
-            if os.path.exists(f"cache/{leagueName}-MAT.json"):
-                print('Reading from cache :D')
-                with open(f"cache/{leagueName}-MAT.json", 'r') as cacheData:
-                    obj = json.load(cacheData)
-            else:
-
-                #Fetching data from API
-                r = requests.get(f"https://api.football-data.org/v2/competitions/{id}/matches?status=SCHEDULED", headers=headers)
-                obj = r.json()
-
-                #Writing data to cache
-                with open(f"cache/{leagueName}-MAT.json", 'w') as cachedata:
-                    json.dump(obj, cachedata, indent=4)
-
-            fixtures = discord.Embed(title = obj['competition']['name'],\
-                            description = 'Fixtures',\
-                            color=0xf58300)
-            if len(obj['matches']) == 0:
-                fixtures.add_field(name = 'No remaining matches in the current season!',\
-                                   value= '\u200b')
-            else:
-                for i, match in enumerate(obj['matches']):
-                    if i > limit - 1:
-                        break
-                    matchTime = dt.datetime.strptime(match['utcDate'][:-1], '%Y-%m-%dt%H:%M:%S')
-                    
-                    #Converting to IST from UTC
-                    matchTime += dt.timedelta(hours = 5, minutes = 30)
-                    homeTeam = match['homeTeam']['name']
-                    awayTeam = match['awayTeam']['name']
-
-                    date = matchTime.strftime('%d %B %Y')
-                    time = matchTime.strftime('%I:%M %p')
-                    fixtures.add_field(name=f'{homeTeam}  :regional_indicator_v::regional_indicator_s:  {awayTeam}',\
-                                        value=f'Date: {date}\nTime: {time}'+ str('\n\u200b' if i != limit - 1 else ' '),
-                                        inline=False)
-                    
-                
-            return fixtures
-
-                
-    except AssertionError:
-        return discord.Embed(title = 'Limit must be greater than :zero:',\
-                            description="Enter a valid limit :smile:",\
-                            color=0xf58300 ) 
-
 
 if __name__ == "__main__":
     print(getStandings('PL'))
